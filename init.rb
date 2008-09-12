aa17d6a7cf9682db8e59b3fe1cd5c55e83ad706e
Matt <matt@matts-macbook.local>
2008-09-12 15:36:52 -0400

    fixed the default args for the before_filter

--- init.rb
+++ init.rb
@@ -4,3 +4,5 @@ require "site_logger"
 ActionController::Base.class_eval do
   include DMT::SiteLogger
 end
+
+ActionView::Base.send :include, DMT::SiteLogger::ViewHelpers
--- lib/site_logger.rb
+++ lib/site_logger.rb
@@ -7,30 +7,42 @@ module DMT
     end
 
     module ClassMethods
-      def dmt_site_logger(args={ :except => '' })
-        self.send(:before_filter, :dmt_site_logger, args)
+      def dmt_site_logger(args=Hash.new())
+        self.send(:after_filter, :dmt_site_logger, args)
         extend DMT::SiteLogger::SingletonMethods
         include DMT::SiteLogger::InstanceMethods
       end
     end
 
     module SingletonMethods
-
     end
 
     module InstanceMethods
-      def dmt_site_logger
-        data = Hash.new
-        data = { 'controller' => params[:controller],
-          'action' => params[:action],
-          'params' => params.to_yaml,
-          'ipaddr' => request.remote_addr,
-          'referer' => request.env['HTTP_REFERER'],
-          'browser' => request.user_agent }
-        log = Dmt::SiteLogger::Log.new(data)
-        log.save
+
+      attr_accessor :dmt_site_logger_flag
+
+      def dmt_site_logger_disable
+        self.dmt_site_logger_flag = true
       end
-    
+
+      def dmt_site_logger
+        if !self.dmt_site_logger_flag
+          data = Hash.new
+          data = { 'controller' => params[:controller],
+            'action' => params[:action],
+            'user' => params[:user],
+            'path_uri' => request.env['REQUEST_URI'],
+            'params' => params.to_yaml,
+            'ipaddr' => request.remote_addr,
+            'referer' => request.env['HTTP_REFERER'],
+            'browser' => request.user_agent }
+          log = Dmt::SiteLogger::Log.new(data)
+          log.save
+        end
+      end
+    end
+
+    module ViewHelpers
       def dmt_site_logger_count
         Dmt::SiteLogger::Log.count
       end
